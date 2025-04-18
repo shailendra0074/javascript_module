78c4594f85f888dc19cdbf3135589815a979e1de
shailendra0074 <infoshailendra0074@gmail.com>
2025-04-18 15:57:33 +0530

    path_module

--- path_module.js
+++ path_module.js
@@ -1,3 +1,17 @@
 const path = require("path");
 console.log(__dirname);
 console.log(__filename);
+
+// school/folder/student/data.txt
+
+const filePath = path.join("folder", "students", "data.txt");
+console.log(filePath);
+
+const parseData = path.parse(filePath);
+const resolvePath = path.resolve(filePath);
+const extName = path.extname(filePath);
+const baseName = path.basename(filePath);
+const dirName = path.dirname(filePath);
+
+
+console.log({parseData, resolvePath, extName, baseName, dirName});
