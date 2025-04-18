0c0822495d29ac5574f2b8f914dddd0a5f6ac52e
shailendra0074 <infoshailendra0074@gmail.com>
2025-04-18 15:30:17 +0530

    common js, destructuring, path_module

---
+++ app.js
@@ -0,0 +1,11 @@
+// common js
+// const add = require("./math");
+
+// console.log(add(5,6));
+
+// const { add, mult } = require("./math");
+
+const math = require("./math");
+
+console.log(math.add(5,6));
+console.log(math.mult(5,6));
---
+++ math.js
@@ -0,0 +1,9 @@
+const add = (a,b) => {
+    return (a+b);
+};
+
+const mult = (a,b) => {
+    return (a*b);
+};
+
+module.exports = { add, mult };
